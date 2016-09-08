7c41fd1546df4579d0637bf3ab3306da4aa90aa7
Dave Lasley <dave@laslabs.com>
2016-09-08 11:51:14 -0700

    [ADD] connector_carepoint: Implement pathology and codes * Add `medical.pathology` bindings and import * Add `medical.pathology.code.type` bindings and import

---
+++ connector_carepoint/models/medical_pathology.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+from openerp import models, fields
+from openerp.addons.connector.connector import ConnectorUnit
+from openerp.addons.connector.unit.mapper import (mapping,
+                                                  only_create,
+                                                  )
+from ..unit.backend_adapter import CarepointCRUDAdapter
+from ..backend import carepoint
+from ..unit.mapper import PartnerImportMapper, trim
+from ..unit.import_synchronizer import (DelayedBatchImporter,
+                                        CarepointImporter,
+                                        )
+
+from ..connector import add_checkpoint
+
+
+_logger = logging.getLogger(__name__)
+
+
+class CarepointMedicalPathology(models.Model):
+    """ Binding Model for the Carepoint Store """
+    _name = 'carepoint.medical.pathology'
+    _inherit = 'carepoint.binding'
+    _inherits = {'medical.pathology': 'odoo_id'}
+    _description = 'Carepoint Pathologies'
+    _cp_lib = 'pathology'
+
+    odoo_id = fields.Many2one(
+        comodel_name='medical.pathology',
+        string='Company',
+        required=True,
+        ondelete='cascade'
+    )
+
+
+class MedicalPathology(models.Model):
+    """ Adds the ``one2many`` relation to the Carepoint bindings
+    (``carepoint_bind_ids``)
+    """
+    _inherit = 'medical.pathology'
+
+    carepoint_bind_ids = fields.One2many(
+        comodel_name='carepoint.medical.pathology',
+        inverse_name='odoo_id',
+        string='Carepoint Bindings',
+    )
+
+
+@carepoint
+class MedicalPathologyAdapter(CarepointCRUDAdapter):
+    """ Backend Adapter for the Carepoint Store """
+    _model_name = 'carepoint.medical.pathology'
+
+
+@carepoint
+class MedicalPathologyBatchImporter(DelayedBatchImporter):
+    """ Import the Carepoint Stores.
+    For every company in the list, a delayed job is created.
+    """
+    _model_name = ['carepoint.medical.pathology']
+
+
+@carepoint
+class MedicalPathologyUnit(ConnectorUnit):
+    _model_name = 'carepoint.medical.pathology'
+
+    def _import_by_code(self, code):
+        adapter = self.unit_for(MedicalPathologyAdapter)
+        importer = self.unit_for(MedicalPathologyImporter)
+        for record in adapter.search(icd_cd=code):
+            importer.run(record)
+
+
+@carepoint
+class MedicalPathologyImportMapper(PartnerImportMapper):
+    _model_name = 'carepoint.medical.pathology'
+
+    direct = [
+        (trim('icd_cd'), 'code'),
+        (trim('icd_desc'), 'name'),
+    ]
+
+    @mapping
+    @only_create
+    def odoo_id(self, record):
+        """ Will bind the record on an existing record with the same name """
+        code_type = self.code_type_id(record)
+        record = self.env['medical.pathology'].search([
+            ('code_type_id', '=', code_type['code_type_id']),
+            ('code', '=', record['icd_cd'].strip()),
+        ],
+            limit=1,
+        )
+        if record:
+            return {'odoo_id': record.id}
+
+    @mapping
+    def code_type_id(self, record):
+        binder = self.binder_for('carepoint.medical.pathology.code.type')
+        type_id = binder.to_odoo(record['icd_cd_type'].strip())
+        return {'code_type_id': type_id}
+
+    @mapping
+    def carepoint_id(self, record):
+        return {'carepoint_id': '%s,%s' % (record['icd_cd'].strip(),
+                                           record['icd_cd_type'].strip(),
+                                           )}
+
+
+@carepoint
+class MedicalPathologyImporter(CarepointImporter):
+    _model_name = ['carepoint.medical.pathology']
+    _base_mapper = MedicalPathologyImportMapper
+
+    def _import_dependencies(self):
+        record = self.carepoint_record
+        self._import_dependency(record['icd_cd_type'].strip(),
+                                'carepoint.medical.pathology.code.type')
+
+    def _create(self, data):   # pragma: no cover
+        binding = super(MedicalPathologyImporter, self)._create(data)
+        add_checkpoint(
+            self.session, binding._name, binding.id, binding.backend_id.id
+        )
+        return binding
---
+++ connector_carepoint/tests/models/test_medical_pathology_type_code.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.addons.connector_carepoint.models import (
+    medical_pathology_code_type,
+)
+
+from ..common import SetUpCarepointBase
+
+
+class EndTestException(Exception):
+    pass
+
+
+class MedicalPathologyCodeTypeTestBase(SetUpCarepointBase):
+
+    def setUp(self):
+        super(MedicalPathologyCodeTypeTestBase, self).setUp()
+        self.model = 'carepoint.medical.pathology.code.type'
+        self.mock_env = self.get_carepoint_helper(
+            self.model
+        )
+        self.record = {
+            'icd_cd_type': ' ICD Type Code ',
+            'icd_cd_type_desc': ' ICD Type Desc ',
+        }
+
+
+class TestMedicalPathologyCodeTypeImportMapper(
+    MedicalPathologyCodeTypeTestBase
+):
+
+    def setUp(self):
+        super(TestMedicalPathologyCodeTypeImportMapper, self).setUp()
+        self.Unit = \
+            medical_pathology_code_type.MedicalPathologyCodeTypeImportMapper
+        self.unit = self.Unit(self.mock_env)
+
+    def test_odoo_id(self):
+        """ It should return odoo_id of pharmacies with same name """
+        model = self.env['medical.pathology.code.type']
+        expect = model.create({
+            'name': self.record['icd_cd_type_desc'].strip(),
+        })
+        res = self.unit.odoo_id(self.record)
+        expect = {'odoo_id': expect.id}
+        self.assertDictEqual(expect, res)
+
+    def test_carepoint_id(self):
+        """ It should return correct attribute """
+        res = self.unit.carepoint_id(self.record)
+        expect = {
+            'carepoint_id': self.record['icd_cd_type'],
+        }
+        self.assertDictEqual(expect, res)
