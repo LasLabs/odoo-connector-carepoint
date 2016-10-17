30bfe04e0fa1386e263cbd0e8dbc6f3689f9cb21
Dave Lasley <dave@laslabs.com>
2016-10-16 19:36:41 -0700

    [FIX] connector_carepoint: Fix prescription migration * Add try/catch & rollback to db alterations in case server re-upgrades

--- connector_carepoint/migrations/9.0.1.3.0/pre-migrate.py
+++ connector_carepoint/migrations/9.0.1.3.0/pre-migrate.py
@@ -2,10 +2,22 @@
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
+_logger = logging.getLogger(__name__)
+
 
 def migrate(cr, version):
-    cr.execute('ALTER TABLE carepoint_medical_prescription_order_line '
-               'RENAME TO carepoint_rx_ord_ln')
+    try:
+        cr.execute('ALTER TABLE carepoint_medical_prescription_order_line '
+                   'RENAME TO carepoint_rx_ord_ln')
+    except Exception:
+        cr.rollback()
+        _logger.exception('Cannot perform migration')
 
-    cr.execute('ALTER TABLE carepoint_carepoint_organization '
-               'RENAME TO carepoint_org_bind')
+    try:
+        cr.execute('ALTER TABLE carepoint_carepoint_organization '
+                   'RENAME TO carepoint_org_bind')
+    except Exception:
+        cr.rollback()
+        _logger.exception('Cannot perform migration')
